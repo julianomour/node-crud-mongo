838b90e12728653237c61f68233342fb9229c25f
Juliano Moura <julianodeveloper@gmail.com>
2018-03-30 20:50:00 -0300

    configuração das rotas get,post,put e delete + configuração local do mongodb

--- index.js
+++ index.js
@@ -9,9 +9,12 @@
  var bodyParser = require('body-parser');
  var mongoose =  require('mongoose');
  var Produto = require('./app/models/produto');
+ mongoose.Promise = global.Promise
 //  URI do MLab
- mongoose.connect('mongodb://juliano:crudapi@ds029496.mlab.com:29496/mongo-crud')
+//  mongoose.connect('mongodb://juliano:crudapi@ds029496.mlab.com:29496/mongo-crud')
 
+// conexão local ao mongodb
+mongoose.connect('mongodb://localhost:27017/node-crud-mongo')
 
 //  Indica que retornaremos dados json atraves do bodyParser()
  app.use(bodyParser.urlencoded({extended : true}))
@@ -40,7 +43,68 @@
          res.json({message: 'Produto cadastrado'})
     })
   })
- 
+
+  //  método para listar produtos em : GET http://localhost:8000/api/produtos
+ .get((req, res) => {
+    Produto.find((error, produtos) => {
+        if(error){
+            res.send(`Ocorreu um err: ${error}`)
+        }
+        res.json({produtos})
+    })
+ })
+
+//  Rotas que irão terminar em "/produtos/produto_id" (serve para  get, post, put e delete)
+ router.route('/produtos/:produto_id')
+
+//  selecionar produto por ID : localhost:8000/produtos/:produto_id
+
+.get((req, res) => {
+    // função para selecionar produto por id, caso não encontre ele retorna erro
+    Produto.findById(req.params.produto_id, (error, produto) => {
+        if(error){
+            res.send(`Produto não encontrado: ${error}`)
+        }
+        res.json({produto})
+
+    })
+})
+
+
+//  atualizar produto por ID : localhost:8000/produtos/:produto_id
+
+.put((req, res) => {
+    // função para selecionar produto por id, caso não encontre ele retorna erro
+    Produto.findById(req.params.produto_id, (error, produto) => {
+        if(error){
+            res.send(`Produto não encontrado: ${error}`)
+        }
+        // altera os dados dos campos
+        produto.nome = req.body.nome
+        produto.preco = req.body.preco
+        produto.descricao = req.body.descricao 
+        
+        // salva o produto com os novos dados
+        produto.save((error) =>{
+            if(error){
+               res.send(`erro ao atualizar o produto: ${error}`)
+            }         
+            res.json({message: 'Produto alterado com sucesso!'})
+        })
+
+    })
+})
+
+
+.delete((req, res) => {
+    // função para deletar produto por id, caso não encontre ele retorna erro
+    Produto.remove({_id: req.params.produto_id},(error) => {
+        if(error){
+            res.send(`Produto não encontrado: ${error}`)
+        }
+        res.json({message:"Produto removido"})       
+    })
+})
 
  router.use((req, res, next) => {
     console.log('executando algo aqui ...')
